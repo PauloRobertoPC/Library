8dd54b78093b4251b55a296541f2ecf9d89f5283
PauloRobertoPC <prpc025@gmail.com>
2022-07-08 16:28:57 -0300

    Adding Extended Euclidean and Diophantine Equations

---
+++ Math/diophantine.cpp
@@ -0,0 +1,33 @@
+//All the functions below run in O(log n)
+
+
+//{gcd(a, b), x, y} such that a*x + b*y = gcd(a,b)
+tuple<int, int, int> extended_euclides(int a, int b){
+    int x = 1, y = 0, x1 = 0, y1 = 1;
+    while(b){
+        int q = a/b;
+        tie(x, x1) = make_tuple(x1, x - q*x1);
+        tie(y, y1) = make_tuple(y1, y - q*y1);
+        tie(a, b) = make_tuple(b, a - q*b);
+    }
+    return {a, x, y};
+}
+
+// {has_solution, x, y, gcd} such that a*x + b*y = c
+tuple<int, int, int, int> diophantine(int a, int b, int c){
+    int g, x, y; tie(g, x, y) = extended_euclides(abs(a), abs(b));
+    if(c%g) return {0, 0, 0, g};
+    x = x*(c/g);
+    y = y*(c/g);
+    if(a < 0) x = -x;
+    if(b < 0) y = -y;
+    return {1, x, y, g};
+}
+
+// x = modular inverse of a mod m: a*x = 1 (mod m)
+int modular_inverse(int a, int m){
+    int ok, x, y, g; tie(ok, x, y, g) = diophantine(a, -m, 1);
+    if(!ok) return -1; //Don't exist a modular inverse for non-coprime numbers
+    x = (((x%m)+m)%m);
+    return x;
+}
